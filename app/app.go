c7196e12443673ad9c68f4a535fd0fd809365310
dvnpremoutlook <dvnpremchowdary@gmail.com>
2024-07-24 01:19:10 -0700

    initial commit

---
+++ app/app.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func Start() {
+	//Define the routes
+	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
+
+	//Starting the server
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module  BankApp/app
+
+go 1.22.5
